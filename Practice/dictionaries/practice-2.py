e1f1db814d5bf59468b059e2300fe6d912aaccda
Aldrin Brillante <aldrinbrillante@Aldrins-MacBook-Pro.local>
2020-09-24 16:59:19 -0700

    practice problems

---
+++ Practice/dictionaries/practice-2.py
@@ -0,0 +1,12 @@
+#TODO: create a function called create_dessert_dictionary that will take in a filename as a string parameter
+
+#TODO: Inside your function create an empty dictionary
+
+#TODO: read the lines of the file in to a list
+
+#TODO: loop through the list and get the dessert as the key and the name as the value, save these to the dictionary
+
+#TODO: return the completed dictionary
+
+#TODO: call the function with "desserts.txt as the parameter" and print out the result
+
