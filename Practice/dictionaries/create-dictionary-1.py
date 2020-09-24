5223a5b06709850d5d55b933c605c120761386ac
Aldrin Brillante <aldrinbrillante@Aldrins-MacBook-Pro.local>
2020-09-24 10:06:27 -0700

    dictionary practice(s)

---
+++ Practice/dictionaries/create-dictionary-1.py
@@ -0,0 +1,16 @@
+'''
+Let's practice creating a dictionary!
+
+Dicitonaries are a collection of key:value pairs.
+'''
+
+# You own an empanada shop and want to store the price of each type (flavor) of empanadas. 
+
+# TODO: Convert the two lists below into a dictionary called menu, where the key is the flavor and the value is the price (as an int)
+
+flavors = ["chicken", "beef", "cheese", "guava"]
+prices = [1.99, 1.99, 1.00, 2.50]
+
+#create the dictionary below
+menu = {"chicken":1.99, "beef":1.99, "cheese":1.00, "guava":2.50}
+
---
+++ Practice/dictionaries/create-dictionary-2.py
@@ -0,0 +1,8 @@
+#TODO: create an empty dictionary and save it to a 
+# variable called my_dictionary
+my_dic = {}
+#TODO: Create a dictionary with key value pairs 
+# representing an int ID for the key and a string name for the 
+# value, save to it a variable called id_dictionary
+
+id_dic = {1234:"Luis", 4321: "JConr", 5678: "Ashraj", 1111: "Aldrin"}
